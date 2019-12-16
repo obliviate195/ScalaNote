bd739985c104a171a7249ca9eacf2374bfd3d8f4
jjh13 <wnsgur195@gmail.com>
2019-12-16 22:26:01 +0900

    chapter5

---
+++ Programming_in_Scala/ch5_note.scala
@@ -0,0 +1,21 @@
+object ch5_note{
+  val hex=0x5
+  val hex2=0x00FF
+  val magic=0xcafebabe
+
+  val tower=35L
+
+  val little: Short=367
+  val littler: Byte=38
+
+  val big=1.2345
+  val bigger=1.2345e1
+
+  val little=1.2345F
+
+  println("""|Welcome to Ultamix 3000.
+              |Type "HELP" for help.""".stripMargin)
+
+  //심볼 리터럴
+  val s='aSymbol
+}
